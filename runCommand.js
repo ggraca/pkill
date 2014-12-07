ff8488c4f9ac5e97ad83bc2b441607b15702c597
Guilherme Graca <ggracase@gmail.com>
2014-12-07 00:29:03 +0000

    Fixed alias to avoid recursion

--- runCommand.js
+++ runCommand.js
@@ -1,38 +1,67 @@
 alias = {};
 
 command = {
-  alias: function(words){
+  a: function(words){
     if(words.length == 0)
-      return
+      return;
     else if(words.length == 1){
       if(words[0] in alias)
-        delete alias[words[0]]
+        delete alias[words[0]];
     }
     else{
-      alias[words[0]] = words.slice(1, words.length).join(" ");
+      var str = words.slice(1, words.length).join(" ");
+      if(handleAlias(str).indexOf(words[0]) != -1)
+        alias[words[0]] = str;
+      else
+        console.log("denied");
     }
   },
-  swipe: function(words){
+  s: function(words){
     for (var name in bitches) {
       if(dist(bitches[name].position, player.position) < 55)
         bitches[name].die();
     }
   },
+  t: function(words){
+    if(words.length == 0)
+      return
+    if(words[0] in bitches){
+      player.position.x = bitches[words[0]].position.x;
+      player.position.y = bitches[words[0]].position.y;
+    }
+  },
 };
 
 
+function handleAlias(str){
+  var words = str.split(" ");
+  var output = []
+
+  for(var i = 0; i < words.length; i++){
+    if(words[i] in alias)
+      output = output.concat(handleAlias(alias[words[i]]));
+    else
+      output = output.concat([words[i]]);
+  }
+
+  return output;
+}
 
 function runCommand(str){
-  words = str.split(" ");
+  console.log(str);
+  words = handleAlias(str);
+
+  for(var i = 0; i < words.length; i++){
+    console.log(words[i]);
+  }
+
+  console.log(words[0]);
 
   if(words[0] in command)
     command[words[0]](words.slice(1, words.length));
 
-  else if(words[0] in alias)
-    runCommand(alias[words[0]] + words.slice(1, words.length).join(" "));
-
-  else if(bitches[str] != null){
-    lookAt(player, bitches[str].position);
-    bitches[str].die();
+  else if(words[0] in bitches){
+    //lookAt(player, bitches[words[0]].position);
+    bitches[words[0]].die();
   }
 }
